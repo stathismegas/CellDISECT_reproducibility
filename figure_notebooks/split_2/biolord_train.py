2dccfd6703b15dae6ed8b30b0b7d41e785f4bbc0
Arian Amani <arianamani.sec@gmail.com>
2024-05-16 13:59:02 +0200

    Add split_2 train script

---
+++ figure_notebooks/split_2/biolord_train.py
@@ -0,0 +1,70 @@
+import biolord
+
+import scanpy as sc
+
+adata = sc.read_h5ad('/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/eraslan_preprocessed1212_split_deg.h5ad')
+cats = ['tissue', 'Sample ID', 'sex', 'Age_bin', 'CoarseCellType']
+split_key = 'split_2'
+
+biolord.Biolord.setup_anndata(
+    adata=adata,
+    ordered_attributes_keys=[],
+    categorical_attributes_keys=cats,
+    layer="counts",
+)
+
+
+module_params = {
+    "decoder_width": 1024,
+    "decoder_depth": 4,
+    "attribute_nn_width": 512,
+    "attribute_nn_depth": 2,
+    "n_latent_attribute_categorical": 4,
+    "gene_likelihood": "nb",
+    "reconstruction_penalty": 1e2,
+    "unknown_attribute_penalty": 1e1,
+    "unknown_attribute_noise_param": 1e-1,
+    "attribute_dropout_rate": 0.1,
+    "use_batch_norm": False,
+    "use_layer_norm": False,
+    "seed": 42,
+}
+
+
+model = biolord.Biolord(
+    adata=adata,
+    n_latent=32,
+    model_name=f"eraslan_biolord_basicSettings_nb_{split_key}",
+    module_params=module_params,
+    train_classifiers=False,
+    split_key=split_key,
+    train_split='train',
+    valid_split='val',
+    test_split='test',)
+
+
+trainer_params = {
+    "n_epochs_warmup": 0,
+    "latent_lr": 1e-4,
+    "latent_wd": 1e-4,
+    "decoder_lr": 1e-4,
+    "decoder_wd": 1e-4,
+    "attribute_nn_lr": 1e-2,
+    "attribute_nn_wd": 4e-8,
+    "step_size_lr": 45,
+    "cosine_scheduler": True,
+    "scheduler_final_lr": 1e-5,
+}
+
+model.train(
+    max_epochs=500,
+    batch_size=512,
+    plan_kwargs=trainer_params,
+    early_stopping=True,
+    early_stopping_patience=20,
+    check_val_every_n_epoch=10,
+    num_workers=1,
+    enable_checkpointing=False,
+)
+
+model.save(f'/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/models/biolord/eraslan_biolord_basicSettings_nb_{split_key}')
