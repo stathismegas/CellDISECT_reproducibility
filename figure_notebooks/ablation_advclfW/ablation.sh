9bc7fe01a72b324938f94496fba6bf1860c76ce1
Arian Amani <arianamani.sec@gmail.com>
2024-05-23 13:18:21 +0200

    add advCLF ablation

---
+++ figure_notebooks/ablation_advclfW/ablation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility/figure_notebooks/ablation_advclfW
+
+advclfweights="0 0.005 0.01 0.05 0.1 0.25 0.4 0.6 0.8 1 1.5 2 3 5 10"
+for d in $advclfweights; do
+    echo "python train_script.py ${d}" | \
+        bsub -J ablation_clfW${d} -G teichlab -o logfile-${d}.out -e logfile-${d}.err -q gpu-normal -n2 -M32000 -R"select[mem>32000] rusage[mem=32000]" -gpu "mode=shared:num=1:gmem=15000"
+    done
