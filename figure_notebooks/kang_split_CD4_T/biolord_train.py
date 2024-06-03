8f1be70a48eb06910ff1b1fe4cc3840981701b9a
Arian Amani <arianamani.sec@gmail.com>
2024-06-03 17:30:59 +0200

    add train script for kang

---
+++ figure_notebooks/kang_split_CD4_T/biolord_train.py
@@ -0,0 +1,76 @@
+import biolord
+
+import scanpy as sc
+
+adata = sc.read_h5ad('/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/kang_normalized_hvg.h5ad')
+adata = adata[adata.layers['counts'].sum(1) != 0].copy()
+cats = ['cell_type', 'condition']
+split_key = 'split_CD4 T'
+
+biolord.Biolord.setup_anndata(
+    adata=adata,
+    ordered_attributes_keys=[],
+    categorical_attributes_keys=cats,
+    layer="counts",
+)
+biolord.Biolord.setup_anndata(
+    adata,
+    ordered_attributes_keys=None,
+    categorical_attributes_keys=cats,
+    retrieval_attribute_key=None,
+)
+
+module_params = {
+    "decoder_width": 1024,
+    "decoder_depth": 4,
+    "attribute_nn_width": 512,
+    "attribute_nn_depth": 2,
+    "n_latent_attribute_categorical": 4,
+    "gene_likelihood": "nb",
+    "reconstruction_penalty": 1e2,
+    "unknown_attribute_penalty": 1e1,
+    "unknown_attribute_noise_param": 1e-1,
+    "attribute_dropout_rate": 0.1,
+    "use_batch_norm": False,
+    "use_layer_norm": False,
+    "seed": 42,
+}
+
+
+model = biolord.Biolord(
+    adata=adata,
+    n_latent=32,
+    model_name=f"kang_biolord_basicSettings_nb_{split_key}",
+    module_params=module_params,
+    train_classifiers=False,
+    split_key=split_key,
+    train_split='train',
+    valid_split='valid',
+    test_split='ood',)
+
+
+trainer_params = {
+    "n_epochs_warmup": 0,
+    "latent_lr": 1e-4,
+    "latent_wd": 1e-4,
+    "decoder_lr": 1e-4,
+    "decoder_wd": 1e-4,
+    "attribute_nn_lr": 1e-2,
+    "attribute_nn_wd": 4e-8,
+    "step_size_lr": 45,
+    "cosine_scheduler": True,
+    "scheduler_final_lr": 1e-5,
+}
+
+model.train(
+    max_epochs=500,
+    batch_size=512,
+    plan_kwargs=trainer_params,
+    early_stopping=True,
+    early_stopping_patience=20,
+    check_val_every_n_epoch=10,
+    num_workers=1,
+    enable_checkpointing=False,
+)
+
+model.save(f'/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/models/biolord/kang_biolord_basicSettings_nb_{split_key}')
---
+++ figure_notebooks/kang_split_CD4_T/train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility/figure_notebooks/kang_split_CD4_T
+
+model_scripts="train_dis2p.py biolord_train.py scDisInFact_train.py"
+for d in $model_scripts; do
+    echo "python ${d}" | \
+        bsub -J ${d} -G teichlab -o logfile-${d}.out -e logfile-${d}.err -q gpu-lotfollahi -n2 -M16000 -R"select[mem>16000] rusage[mem=16000]" -gpu "mode=shared:num=1:gmem=12000"
+    done
