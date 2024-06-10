8ce4694331e3a55ac633eb98d9923f5d2b347be9
Arian Amani <arianamani.sec@gmail.com>
2024-06-10 14:35:35 +0200

    fix bug

--- figure_notebooks/kang/ablation_cfW/ablation.sh
+++ figure_notebooks/kang/ablation_cfW/ablation.sh
@@ -5,9 +5,9 @@ cd /lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility/fig
 cfweights="0 0.005 0.01 0.03 0.05 0.08 0.1 0.3 0.5 0.8 1 5 10"
 split_key=("split_CD4 T" "split_CD14 Mono")
 
-i = 0
+i=0
 for split in "${split_key[@]}"; do
-    i = i + 1
+    i=$((i + 1))
     for d in $cfweights; do
         echo "python train_script.py ${d} " \"${split}\"" | \
             bsub -J ablation_cfW${d}_${i} -G teichlab -o logfile-${d}_${i}.out -e logfile-${d}_${i}.err -q gpu-lotfollahi -n2 -M16000 -R"select[mem>16000] rusage[mem=16000]" -gpu "mode=shared:num=1:gmem=10000"
--- figure_notebooks/kang/ablation_clfW/ablation.sh
+++ figure_notebooks/kang/ablation_clfW/ablation.sh
@@ -5,9 +5,9 @@ cd /lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility/fig
 cfweights="0 0.005 0.01 0.05 0.1 0.25 0.4 0.6 0.8 1 1.5 2 3 5 10"
 split_key=("split_CD4 T" "split_CD14 Mono")
 
-i = 0
+i=0
 for split in "${split_key[@]}"; do
-    i = i + 1
+    i=$((i + 1))
     for d in $cfweights; do
         echo "python train_script.py ${d} " \"${split}\"" | \
             bsub -J ablation_clfW${d}_${i} -G teichlab -o logfile-${d}_${i}.out -e logfile-${d}_${i}.err -q gpu-lotfollahi -n2 -M32000 -R"select[mem>32000] rusage[mem=32000]" -gpu "mode=shared:num=1:gmem=15000"
