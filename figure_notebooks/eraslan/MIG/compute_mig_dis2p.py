f30b0b1cb74703860e2798d26accf4e3a8819560
Arian Amani <arianamani.sec@gmail.com>
2024-06-29 17:05:00 +0200

    add chdir

--- figure_notebooks/eraslan/MIG/compute_mig_dis2p.py
+++ figure_notebooks/eraslan/MIG/compute_mig_dis2p.py
@@ -15,6 +15,7 @@ import warnings
 
 import sys
 sys.path.insert(1, '/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility')
+os.chdir('/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility')
 from metrics.metrics import Mixed_KSG_MI_metrics, create_cats_idx
 
 adata = sc.read_h5ad('../eraslan_preprocessed1212_split_deg.h5ad')
