773df145b120713d6102a2041bfd01ef2203dc9a
Arian Amani <arianamani.sec@gmail.com>
2024-07-08 18:21:42 +0200

    add better namings for bsub

--- reproduce_benchmarks/eraslan/split_2/train.sh
+++ reproduce_benchmarks/eraslan/split_2/train.sh
@@ -5,5 +5,5 @@ cd /lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility/rep
 model_scripts="train_dis2p.py biolord_train.py scDisInFact_train.py"
 for d in $model_scripts; do
     echo "python ${d}" | \
-        bsub -J ${d} -G teichlab -o logfile-${d}.out -e logfile-${d}.err -q gpu-lotfollahi -n2 -M32000 -R"select[mem>32000] rusage[mem=32000]" -gpu "mode=shared:num=1:gmem=40000"
+        bsub -J ${d}_split2 -G teichlab -o logfile-${d}_split2.out -e logfile-${d}_split2.err -q gpu-lotfollahi -n1 -M32000 -R"select[mem>32000] rusage[mem=32000]" -gpu "mode=shared:num=1:gmem=40000"
     done
