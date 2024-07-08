2d25290d9c004e7328e2f21c8a2b8d77ff9069fe
Arian Amani <arianamani.sec@gmail.com>
2024-07-08 18:52:58 +0200

    kang all splits train code

---
+++ reproduce_benchmarks/kang/biolord_train.py
@@ -0,0 +1,57 @@
+import sys, importlib
+split_key = sys.argv[1]
+
+from pathlib import Path
+
+file = Path(__file__).resolve()
+level=3
+parent, top = file.parent, file.parents[level]
+sys.path.append(str(top))
+try:
+    sys.path.remove(str(parent))
+except ValueError: # already removed
+    pass
+__package__ = '.'.join(parent.parts[len(top.parts):])
+importlib.import_module(__package__)
+
+from ..parameters.biolord_params import train_method_params, n_latent, module_params, trainer_params
+
+import biolord
+
+import scanpy as sc
+
+adata = sc.read_h5ad('/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/kang_normalized_hvg.h5ad')
+adata = adata[adata.layers['counts'].sum(1) != 0].copy()
+cats = ['cell_type', 'condition']
+
+biolord.Biolord.setup_anndata(
+    adata=adata,
+    ordered_attributes_keys=[],
+    categorical_attributes_keys=cats,
+    layer="counts",
+)
+
+model = biolord.Biolord(
+    adata=adata,
+    n_latent=n_latent,
+    model_name=f"eraslan_biolord_basicSettings_nb_{split_key}",
+    module_params=module_params,
+    train_classifiers=False,
+    split_key=split_key,
+    train_split='train',
+    valid_split='valid',
+    test_split='ood',)
+
+model.train(
+    max_epochs=train_method_params["max_epochs"],
+    batch_size=train_method_params["batch_size"],
+    plan_kwargs=trainer_params,
+    early_stopping=train_method_params["early_stopping"],
+    early_stopping_patience=train_method_params["early_stopping_patience"],
+    check_val_every_n_epoch=train_method_params["check_val_every_n_epoch"],
+    num_workers=train_method_params["num_workers"],
+    enable_checkpointing=train_method_params["enable_checkpointing"],
+)
+
+
+model.save(f'/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/models/biolord/kang_biolord_earlierStop_basicSettings_nb_{split_key}')
---
+++ reproduce_benchmarks/kang/train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/dis2p_reproducibility/reproduce_benchmarks/kang
+
+model_scripts="train_dis2p.py biolord_train.py scDisInFact_train.py"
+split_key=("split_CD14 Mono" "split_CD4 T" "split_T" "split_CD8 T" "split_B" "split_DC" "split_CD16 Mono" "split_NK")
+
+for split in "${split_key[@]}"; do
+    for d in $model_scripts; do
+        echo "python ${d} \"${split}\"" | \
+            bsub -J ${d}_\"${split}\" -G teichlab -o logfile-${d}_\"${split}\".out -e logfile-${d}_\"${split}\".err -q gpu-lotfollahi -n1 -M16000 -R"select[mem>16000] rusage[mem=16000]" -gpu "mode=shared:num=1:gmem=12000"
+        done
