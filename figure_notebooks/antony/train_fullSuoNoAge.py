4a61619f176c012acb1872c271aa37aca626be61
Arian Amani <arianamani.sec@gmail.com>
2024-07-22 17:54:15 +0200

    train on the full suo dataset

---
+++ figure_notebooks/antony/train_fullSuoNoAge.py
@@ -0,0 +1,106 @@
+import os
+import shutil
+
+import scvi
+import scanpy as sc
+import torch
+from lightning.pytorch.loggers import WandbLogger
+
+torch.set_float32_matmul_precision('medium')
+import warnings
+warnings.simplefilter("ignore", UserWarning)
+
+from dis2p import dis2pvi_cE as dvi
+
+scvi.settings.seed = 42
+print(torch.cuda.is_available())
+
+data_path = '/lustre/scratch126/cellgen/team298/ar32/farm_job_objects_and_outputs_temp/dis2p_20240703_suo/prepare_data/dis2p_suo_prepped.h5ad'
+
+adata = sc.read(data_path)
+adata = adata[adata.X.sum(1) != 0].copy()
+
+cats = ['integration_donor', 'integration_library_platform_coarse',
+        'organ']
+
+
+arch_dict = {'n_layers': 2,
+ 'n_hidden': 128,
+ 'n_latent_shared': 32,
+ 'n_latent_attribute': 32,
+ 'dropout_rate': 0.2,
+ 'weighted_classifier': True,
+}
+train_dict = {
+ 'max_epochs': 1000,
+ 'batch_size': 1024,
+ 'recon_weight': 19.18,
+ 'cf_weight': 1.158,
+ 'beta': 0.003,
+ 'clf_weight': 0.028,
+ 'adv_clf_weight': 0.014,
+ 'adv_period': 5,
+ 'n_cf': 1,
+ 'early_stopping_patience': 15,
+ 'early_stopping': True,
+ 'save_best': True,
+ 'kappa_optimizer2': False,
+ 'n_epochs_pretrain_ae': 0,
+}
+
+plan_kwargs = {
+ 'lr': 0.00255,
+ 'weight_decay': 0.0000205,
+ 'new_cf_method': True,
+ 'lr_patience': 6,
+ 'lr_factor': 0.5,
+ 'lr_scheduler_metric': 'loss_validation',
+ 'n_epochs_kl_warmup': 10,
+}
+
+
+module_name = f'Antony_suoFull_NoAge'
+pre_path = f'/lustre/scratch126/cellgen/team205/aa34/Arian/Dis2P/models/{module_name}'
+if not os.path.exists(pre_path):
+    os.makedirs(pre_path)
+
+
+# specify a name for your model
+model_name = (
+    f'pretrainAE_{train_dict["n_epochs_pretrain_ae"]}_'
+    f'maxEpochs_{train_dict["max_epochs"]}_'
+    f'reconW_{train_dict["recon_weight"]}_'
+    f'cfWeight_{train_dict["cf_weight"]}_'
+    f'beta_{train_dict["beta"]}_'
+    f'clf_{train_dict["clf_weight"]}_'
+    f'adv_{train_dict["adv_clf_weight"]}_'
+    f'advp_{train_dict["adv_period"]}_'
+    f'n_cf_{train_dict["n_cf"]}_'
+    f'lr_{plan_kwargs["lr"]}_'
+    f'wd_{plan_kwargs["weight_decay"]}_'
+    f'new_cf_{plan_kwargs["new_cf_method"]}_'
+    f'dropout_{arch_dict["dropout_rate"]}_'
+    f'n_hidden_{arch_dict["n_hidden"]}_'
+    f'n_latent_{arch_dict["n_latent_shared"]}_'
+    f'n_layers_{arch_dict["n_layers"]}'
+)
+wandb_logger = WandbLogger(project=f"Antony_Dis2PVI_cE_suoFullNoAge", name=model_name)
+train_dict['logger'] = wandb_logger
+wandb_logger.experiment.config.update({'train_dict': train_dict, 'arch_dict': arch_dict, 'plan_kwargs': plan_kwargs})
+try: # Clean up the directory if it exists, overwrite the model
+    shutil.rmtree(f"{pre_path}/{model_name}")
+    print("Directory deleted successfully")
+except OSError as e:
+    print(f"Error deleting directory: {e}") 
+
+dvi.Dis2pVI_cE.setup_anndata(
+    adata,
+    layer='counts',
+    categorical_covariate_keys=cats,
+    continuous_covariate_keys=[]
+)
+model = dvi.Dis2pVI_cE(adata,
+                       **arch_dict)
+model.train(**train_dict, plan_kwargs=plan_kwargs, )
+model.save(f"{pre_path}/{model_name}", overwrite=True)
+print(model_name)
